c79565ef86531f580502552bef42770936ce7900
Björn Schmidtke <bs@pa1nd.de>
2020-03-13 16:22:40 -0300

    fix navigation

--- src/layouts/NavigationBar.js
+++ src/layouts/NavigationBar.js
@@ -7,21 +7,13 @@ const NavigationBar = () => {
         <div className="bar bar--sm visible-xs">
           <div className="container">
             <div className="row">
-              <div className="col-3 col-md-2">
-                <a href="index.html">
-                  <img
-                    className="logo logo-dark"
-                    alt="logo"
-                    src="img/logo-dark.png"
-                  />
-                  <img
-                    className="logo logo-light"
-                    alt="logo"
-                    src="img/logo-light.png"
-                  />
-                </a>
+              <div className="col-4">
+                <h4>
+                  CoronaVirus
+                  <br /> Auto Report
+                </h4>
               </div>
-              <div className="col-9 col-md-10 text-right">
+              <div className="col-8 text-right">
                 <a
                   href="#"
                   className="hamburger-toggle"
@@ -39,102 +31,16 @@ const NavigationBar = () => {
         >
           <div className="container">
             <div className="row">
-              <div className="col-lg-1 col-md-2 hidden-xs">
-                <div className="bar__module">
-                  <a href="index.html">
-                    <img
-                      className="logo logo-dark"
-                      alt="logo"
-                      src="img/logo-dark.png"
-                    />
-                    <img
-                      className="logo logo-light"
-                      alt="logo"
-                      src="img/logo-light.png"
-                    />
-                  </a>
-                </div>
+              <div className="col-lg-3 col-md-3 hidden-xs">
+                <h4>CoronaVirus Auto Report</h4>
               </div>
-              <div className="col-lg-11 col-md-12 text-right text-left-xs text-left-sm">
+              <div className="col-lg-9 col-md-9 text-right text-left-xs text-left-sm">
                 <div className="bar__module">
                   <ul className="menu-horizontal text-left">
                     <li>
                       <a href="#">Single Link</a>
                     </li>
-                    <li className="dropdown">
-                      <span className="dropdown__trigger">Dropdown Slim</span>
-                      <div className="dropdown__container">
-                        <div className="container">
-                          <div className="row">
-                            <div className="dropdown__content col-lg-2">
-                              <ul className="menu-vertical">
-                                <li>
-                                  <a href="#">Single Link</a>
-                                </li>
-                              </ul>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </li>
-                    <li className="dropdown">
-                      <span className="dropdown__trigger">Dropdown Wide</span>
-                      <div className="dropdown__container">
-                        <div className="container">
-                          <div className="row">
-                            <div className="dropdown__content row w-100">
-                              <div className="col-lg-3">
-                                <h5>Menu Title</h5>
-                                <ul className="menu-vertical">
-                                  <li>
-                                    <a href="#">Single Link</a>
-                                  </li>
-                                </ul>
-                              </div>
-                              <div className="col-lg-3">
-                                <h5>Menu Title</h5>
-                                <ul className="menu-vertical">
-                                  <li>
-                                    <a href="#">Single Link</a>
-                                  </li>
-                                </ul>
-                              </div>
-                              <div className="col-lg-3">
-                                <h5>Menu Title</h5>
-                                <ul className="menu-vertical">
-                                  <li>
-                                    <a href="#">Single Link</a>
-                                  </li>
-                                </ul>
-                              </div>
-                              <div className="col-lg-3">
-                                <h5>Menu Title</h5>
-                                <ul className="menu-vertical">
-                                  <li>
-                                    <a href="#">Single Link</a>
-                                  </li>
-                                </ul>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </li>
                   </ul>
-                </div>
-                <div className="bar__module">
-                  <a
-                    className="btn btn--sm type--uppercase"
-                    href="#customise-template"
-                  >
-                    <span className="btn__text">Try Builder</span>
-                  </a>
-                  <a
-                    className="btn btn--sm btn--primary type--uppercase"
-                    href="#purchase-template"
-                  >
-                    <span className="btn__text">Buy Now</span>
-                  </a>
                 </div>
               </div>
             </div>
