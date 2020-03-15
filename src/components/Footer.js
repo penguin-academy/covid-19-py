469267c779a623b4cbc11a146e2c1ffe697a8838
Björn Schmidtke <bs@pa1nd.de>
2020-03-15 20:30:59 -0300

    style form wraper. move header and footer into components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -73,48 +73,29 @@ const Footer = () => {
       </div>
     </footer>
   )
+}
 
-  // return (
-  //   <footer className="footer pt-10 pb-5 mt-auto bg-primary footer-dark">
-  //     <div className="container">
-  //       <div className="row">
-  //         <div className="col-lg-3">
-  //           <div className="footer-brand">CovPY Auto Reporte</div>
-  //           <div className="mb-3">Resguardando la salud paraguaya</div>
-  //           <div className="icon-list-social mb-5">
-  //             <a className="icon-list-social-link" href="/">
-  //               <i className="fab fa-instagram" />
-  //             </a>
-  //             <a className="icon-list-social-link" href="/">
-  //               <i className="fab fa-facebook" />
-  //             </a>
-  //             <a className="icon-list-social-link" href="/">
-  //               <i className="fab fa-github" />
-  //             </a>
-  //             <a className="icon-list-social-link" href="/">
-  //               <i className="fab fa-twitter" />
-  //             </a>
-  //           </div>
-  //         </div>
-  //       </div>
-  //       <hr className="my-5" />
-  //       <div className="row align-items-center">
-  //         <div className="col-md-6 small">
-  //           Copyright © CovPY 2020 - made with
-  //           <img
-  //             src="/img/heart.svg"
-  //             alt="Heart"
-  //             style={{ padding: '0 10px', maxWidth: 45 }}
-  //           />
-  //           in Paraguay
-  //         </div>
-  //         <div className="col-md-6 text-md-right small">
-  //           <Link to="/legal">Privacy · Terms &amp; Conditions</Link>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </footer>
-  // )
+export const FooterSmall = () => {
+  return (
+    <footer className="fdb-block footer-small bg-dark">
+      <div className="container">
+        <div className="row text-center">
+          <div className="col">
+            <p>
+              {' '}
+              © CovPY 2020 - made with
+              <img
+                src="/img/heart.svg"
+                alt="Heart"
+                style={{ padding: '0 10px', maxWidth: 45 }}
+              />
+              in Paraguay
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
