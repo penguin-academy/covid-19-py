654354e30fddedebe21b93637cc332e3bbb1792a
Björn Schmidtke <bs@pa1nd.de>
2020-03-19 02:12:20 -0300

    small fixes

--- src/components/AlignmentChart.js
+++ src/components/AlignmentChart.js
@@ -1,36 +1,52 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from 'react'
 import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
-} from 'recharts';
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from 'recharts'
 
-var data = null;
-
-
-
+var data = null
 
 export default class AlignmentChart extends PureComponent {
-  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/xqjtetw0/';
+  // static jsfiddleUrl = 'https://jsfiddle.net/alidingling/xqjtetw0/'
 
   constructor() {
     super()
     this.state = {
-      data:[],
-      loaded:false,
-      colours:["#F78378","#FB8599","#F18FBA","#DA9FD6","#B7B0EA","#8BC1F1","#5CCEEA","#3BD9D7","#4AE0BB","#73E49B","#A0E57C","#CEE164"]
+      data: [],
+      loaded: false,
+      colours: [
+        '#F78378',
+        '#FB8599',
+        '#F18FBA',
+        '#DA9FD6',
+        '#B7B0EA',
+        '#8BC1F1',
+        '#5CCEEA',
+        '#3BD9D7',
+        '#4AE0BB',
+        '#73E49B',
+        '#A0E57C',
+        '#CEE164'
+      ]
     }
-
   }
 
   componentDidMount() {
-    fetch('http://localhost:8000/alignment.json')
+    fetch('https://penguin-utility-server.herokuapp.com/covid19/alignment.json')
       .then(response => response.json())
-      .then((jsonData) => {
+      .then(jsonData => {
         // jsonData is parsed json object received from url
-        this.setState({data:jsonData});
-        this.setState({loaded:true});
+        this.setState({ data: jsonData })
+        this.setState({ loaded: true })
         console.log(jsonData)
       })
-      .catch((error) => {
+      .catch(error => {
         // handle your errors here
         console.error(error)
       })
@@ -38,7 +54,12 @@ export default class AlignmentChart extends PureComponent {
 
   render() {
     if (this.state.loaded == false)
-      return (<h1>loading..</h1>)
+      return (
+        <h2 className="text-center m-5">
+          <i className="fas fa-spinner fa-pulse mr-3"></i>
+          loading...
+        </h2>
+      )
     else
       return (
         <div style={{ width: '100%', height: 450 }}>
@@ -46,20 +67,33 @@ export default class AlignmentChart extends PureComponent {
             <LineChart
               data={this.state.data}
               margin={{
-                top: 5, right: 30, left: 20, bottom: 5,
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5
               }}
             >
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="x" type="number" allowDuplicatedCategory={false} />
-              <YAxis dataKey="y" type="number"/>
+              <XAxis
+                dataKey="x"
+                type="number"
+                allowDuplicatedCategory={false}
+              />
+              <YAxis dataKey="y" type="number" />
               <Tooltip />
-              <Legend layout='vertical' align='right' verticalAlign='top'/>
-              {this.state.data.country_data.map((s,i) => (
-                <Line dataKey="y" data={s.data} name={s.name + " ("+s.offset+" detrás de Brasil)"} stroke={this.state.colours[i]} key={s.name} />
+              <Legend layout="vertical" align="right" verticalAlign="top" />
+              {this.state.data.country_data.map((s, i) => (
+                <Line
+                  dataKey="y"
+                  data={s.data}
+                  name={s.name + ' (' + s.offset + ' detrás de Brasil)'}
+                  stroke={this.state.colours[i]}
+                  key={s.name}
+                />
               ))}
             </LineChart>
           </ResponsiveContainer>
         </div>
-      );
+      )
   }
-};
+}
---
+++ src/stories/5-Charts.stories.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import AlignmentChartTag from '../components/AlignmentChart'
+
+export default {
+  title: 'Charts'
+}
+
+export const AlignmentChart = () => (
+  <div style={{ margin: 15 }}>
+    <AlignmentChartTag />
+  </div>
+)
